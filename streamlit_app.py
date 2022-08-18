905291ef2d2beb602a1ebb1b85213554285a0b2f
Reeree <99715617+WildReeree@users.noreply.github.com>
2022-08-18 17:17:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+
+
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
