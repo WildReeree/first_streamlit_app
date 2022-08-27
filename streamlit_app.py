b76d9a200b7551f55cdfa00bb6187e55e05f4e38
Reeree <99715617+WildReeree@users.noreply.github.com>
2022-08-27 19:21:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 #Normalises the values into consistent columns
